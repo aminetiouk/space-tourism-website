fa054cb4f0f14c2b67f6bde9b946ecc399e9cfae
aminetiouk <tiouk.amine@gmail.com>
2025-05-01 00:13:14 +0100

    feat(App): setup routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home'
+import Destination from './components/Destination';
+import Crew from './components/Crew';
+import Technology from './components/Technology';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
