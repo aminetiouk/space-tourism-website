dc96649ea55c1d3d0773443af3b2a63e29c26759
aminetiouk <tiouk.amine@gmail.com>
2025-04-30 23:15:22 +0100

    feat(ui): add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import Crew from "./Crew";
+import Destination from "./Destination";
+import Home from "./Home";
+import Technology from "./Technology";
+
+export default function NavBar() {
+  return (
+    <header>
+      <img src="" alt="" />
+      <nav>
+        <Home />
+        <Destination />
+        <Crew />
+        <Technology />
+      </nav>
+    </header>
+  )
+}
