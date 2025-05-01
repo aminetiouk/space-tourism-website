74df3fc803becaf8ba23dda68434067fa203414d
aminetiouk <tiouk.amine@gmail.com>
2025-05-01 12:44:21 +0100

    feat(home): implement hero layout with background image and text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom';
+import background from 'src/assets/home/background-home-desktop.jpg';
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className='home'>
+      <img src={background} alt="background" className="home-background" />
+      <div className="hero">
+        <section className="hero-text">
+          <p>SO, YOU WANT TO TRAVEL TO</p>
+          <p>SPACE</p>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </section>
+
+        <Link to="/destination">
+          <button className="hero-button">EXPLORE</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
