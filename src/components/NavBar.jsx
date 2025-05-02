dfcd2a0995c12efdf6662b2a3de37857e1d9465b
aminetiouk <tiouk.amine@gmail.com>
2025-05-02 22:27:44 +0100

    refactor(navbar): restructure navbar content layout for better organization

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,22 +4,24 @@ import logo from '../assets/shared/logo.svg';
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <img src={logo} alt="logo" className="nav-logo" />
-      <div className="nav-line"></div>
-      <ul className="nav-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/destination">Destination</Link>
-        </li>
-        <li>
-          <Link to="/crew">Crew</Link>
-        </li>
-        <li>
-          <Link to="/technology">Technology</Link>
-        </li>
-      </ul>
+      <div className="nav-content">
+        <img src={logo} alt="logo" className="nav-logo" />
+        <div className="nav-line"></div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/destination">Destination</Link>
+          </li>
+          <li>
+            <Link to="/crew">Crew</Link>
+          </li>
+          <li>
+            <Link to="/technology">Technology</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
