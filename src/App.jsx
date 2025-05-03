72dc5180dd46df3e7445ffc2234a4751665c09c3
aminetiouk <tiouk.amine@gmail.com>
2025-05-03 20:32:31 +0100

    refactor(App): improve component structure and logic in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Home from './components/Home'
-import Crew from './components/crew/Crew'
-import Technology from './components/technology/Technology'
+import Home from './components/Home';
+import Crew from './components/crew/Crew';
+import Technology from './components/technology/Technology';
 import Destination from './components/destination/Destination';
 import Background from './components/Background';
 import Navbar from './components/Navbar';
+import { AnimatePresence} from 'framer-motion';
+import PageWrapper from './components/PageWrapper';
 
 function App() {
-
   const location = useLocation();
   const page = location.pathname.split('/')[1] || 'home';
 
   return (
     <>
+      <PageWrapper page={page}>
+        <Background page={page} />
+      </PageWrapper>
+
       <Navbar page={page} />
-      <Background page={page} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/crew" element={<Crew />} />
-        <Route path="/technology" element={<Technology />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
