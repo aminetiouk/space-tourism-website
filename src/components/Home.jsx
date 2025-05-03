3cbfdde3d079258acc3841c35120163c87affa8c
aminetiouk <tiouk.amine@gmail.com>
2025-05-03 15:04:28 +0100

    style(home): apply text presets, improve layout and spacing for hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div className="home">
       <div className="hero">
         <section className="hero-text">
-          <p>SO, YOU WANT TO TRAVEL TO</p>
-          <p>SPACE</p>
-          <p>
+          <p className="text-preset-5">SO, YOU WANT TO TRAVEL TO</p>
+          <p className="text-preset-1">SPACE</p>
+          <p className="text-preset-9">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
@@ -16,7 +16,7 @@ export default function Home() {
         </section>
 
         <Link to="/destination">
-          <button className="hero-button">EXPLORE</button>
+          <button className="hero-button text-preset-4">EXPLORE</button>
         </Link>
       </div>
     </div>
