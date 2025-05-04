1bba1826d32b55cfd66cb441ab373fe7a0a3785c
aminetiouk <tiouk.amine@gmail.com>
2025-05-04 15:40:22 +0100

    chore(vite): remove unused path alias for 'src'

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, 'src'),
-    },
-  },
 });
