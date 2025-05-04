3fbf3438869070ae75dc914af1dd7890bfcb020f
aminetiouk <tiouk.amine@gmail.com>
2025-05-04 15:03:16 +0100

    refactor: update Background component with types

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,3 +1,8 @@
-export default function Background({ children, page }) {
+type BackgroundProps = {
+  children?: React.ReactNode;
+  page: string;
+};
+
+export default function Background({ children, page }: BackgroundProps) {
   return <div className={`background background--${page}`}>{children}</div>;
 }
