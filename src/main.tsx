f4b3bb0daa84c64bd0625d601274a81a686252f3
aminetiouk <tiouk.amine@gmail.com>
2025-05-04 15:03:16 +0100

    fix: check for root container before rendering

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,11 @@ import App from './App.jsx';
 import './App.css'
 import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root')
+
+if (!container) throw new Error("Root container not found");
+
+createRoot(container).render(
   <StrictMode>
     <BrowserRouter>
       <App />
