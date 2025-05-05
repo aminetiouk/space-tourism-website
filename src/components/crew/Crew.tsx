e5910c27d81168759cbbcaa987524a7089480614
aminetiouk <tiouk.amine@gmail.com>
2025-05-05 22:40:01 +0100

    refactor: simplify CrewCard props and improve layout separation

--- src/components/crew/Crew.tsx
+++ src/components/crew/Crew.tsx
@@ -3,81 +3,64 @@ import data from '../../lib/data.json';
 import { useState } from 'react';
 
 type TCrewCardProps = {
-  crews: {
-    role: string;
-    name: string;
-    bio: string;
-    images: { webp: string };
-  }[];
   role: string;
   name: string;
   bio: string;
-  currentIndex: number;
-  setCurrentIndex: React.Dispatch<React.SetStateAction<number>>;
   imageKey: string;
 };
 
-function CrewCard({
-  crews,
-  role,
-  name,
-  bio,
-  currentIndex,
-  setCurrentIndex,
-  imageKey
-}: TCrewCardProps) {
+function CrewCard({ role, name, bio, imageKey }: TCrewCardProps) {
   const imgSrc = `/assets/crew/${imageKey}`;
+  console.log(imageKey);
   return (
     <div className="crew">
-      <div className="crew-content">
-        <div className="crew-info">
-          <h3 className="text-preset-4">{role}</h3>
-          <h2 className="text-preset-3">{name}</h2>
-          <p className="text-preset-9">{bio}</p>
-        </div>
-
-        <nav>
-          <ul className="crew-nav">
-            {crews.map((crew, index) => (
-              <li key={crew.name}>
-                <button
-                  className={`crew-button ${index === currentIndex ? 'crew-button__active' : ''}`}
-                  onClick={() => setCurrentIndex(index)}
-                ></button>
-              </li>
-            ))}
-          </ul>
-        </nav>
+      <div className="crew-info">
+        <h3 className="text-preset-4">{role}</h3>
+        <h2 className="text-preset-3">{name}</h2>
+        <p className="text-preset-9">{bio}</p>
       </div>
-      <div>
-        <img src={imgSrc} alt="crew image" className="crew-image" />
+      <div className="image-container">
+        <img
+          src={imgSrc}
+          alt="crew image"
+          className={`crew-image ${imageKey === 'image-mark-shuttleworth.webp' ? 'crew-image__edit' : ''}`}
+        />
       </div>
     </div>
   );
 }
 
 export default function Crew() {
-  const crew = data.crew;
+  const crews = data.crew;
   const [currentIndex, setCurrentIndex] = useState(0);
-  const currentCrew = crew[currentIndex];
+  const currentCrew = crews[currentIndex];
   const imageKey = currentCrew.images.webp.split('/').pop() ?? '';
 
   return (
-    <PageWrapper>
-      <div className="crew-container">
-        <h1 className="text-preset-5">
-          <span className="title-number">02</span> MEET YOUR CREW
-        </h1>
+    <div className="crew-container">
+      <h1 className="text-preset-5">
+        <span className="title-number">02</span> MEET YOUR CREW
+      </h1>
+      <nav>
+        <ul className="crew-nav">
+          {crews.map((crew, index) => (
+            <li key={crew.name}>
+              <button
+                className={`crew-button ${index === currentIndex ? 'crew-button__active' : ''}`}
+                onClick={() => setCurrentIndex(index)}
+              ></button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <PageWrapper page={`crew-${currentIndex}`}>
         <CrewCard
-          crews={crew}
           role={currentCrew.role.toUpperCase()}
           name={currentCrew.name.toUpperCase()}
           bio={currentCrew.bio}
-          currentIndex={currentIndex}
-          setCurrentIndex={setCurrentIndex}
           imageKey={imageKey}
         />
-      </div>
-    </PageWrapper>
+      </PageWrapper>
+    </div>
   );
 }
