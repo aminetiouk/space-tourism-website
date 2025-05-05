34d6d38779505e704ccdc05ead678c03129d411b
aminetiouk <tiouk.amine@gmail.com>
2025-05-06 00:40:41 +0100

    feat: add Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,12 @@
+type TitleProps = {
+  number: string;
+  text: string;
+}
+
+export default function Title({ number, text }: TitleProps) {
+  return (
+    <h1 className="text-preset-5">
+      <span className="title-number">{number}</span>{` ${text}`}
+    </h1>
+  );
+}
