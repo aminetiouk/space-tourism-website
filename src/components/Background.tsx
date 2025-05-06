b42b2083fc8f44d751e526a4001a8a464fbbb324
aminetiouk <tiouk.amine@gmail.com>
2025-05-06 02:06:38 +0100

    feat: animate background transition based on route using framer-motion

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,8 +1,21 @@
+import { motion } from 'framer-motion';
+
 type BackgroundProps = {
   children?: React.ReactNode;
   page: string;
 };
 
 export default function Background({ children, page }: BackgroundProps) {
-  return <div className={`background background--${page}`}>{children}</div>;
+  return (
+    <motion.div
+    key={page}
+    className={`background background--${page}`}
+    initial={{ opacity: 0.1 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0.1 }}
+    transition={{ duration: 1, ease: 'easeInOut' }}
+  >
+    {children}
+  </motion.div>
+  );
 }
