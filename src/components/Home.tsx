b52a10853b8e76b68b5bf76935b1d00cc0ccc8b3
aminetiouk <tiouk.amine@gmail.com>
2025-05-09 23:25:03 +0100

    refactor(home): simplify layout by removing PageWrapper and applying BEM class naming

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,23 @@
 import { Link } from 'react-router-dom';
-import PageWrapper from './PageWrapper';
 
 export default function Home() {
   return (
-    <PageWrapper>
-      <div className="home">
-        <div className="hero">
-          <section className="hero-text">
-            <p className="text-preset-5">SO, YOU WANT TO TRAVEL TO</p>
+        <div className="home__container">
+          <section className="home__content">
+            <p className="text-preset-5 text-blue-300">SO, YOU WANT TO TRAVEL TO</p>
             <p className="text-preset-1">SPACE</p>
-            <p className="text-preset-9">
+            <p className="text-preset-9 text-blue-300">
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
               this world experience!
             </p>
           </section>
-          <div className="button">
+          <div className="home__button-container">
             <Link to="/destination">
-              <button className="hero-button text-preset-4">EXPLORE</button>
+              <button className="home__button text-preset-4">EXPLORE</button>
             </Link>
           </div>
         </div>
-      </div>
-    </PageWrapper>
   );
 }
