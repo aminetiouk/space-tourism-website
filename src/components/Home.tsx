01d148504d227bff6b95446a95e82e3b93440f91
aminetiouk <tiouk.amine@gmail.com>
2025-05-10 01:10:35 +0100

    refactor(home): update content structure and improve accessibility with new classes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,22 +2,22 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-        <div className="home__container">
-          <section className="home__content">
-            <p className="text-preset-5 text-blue-300">SO, YOU WANT TO TRAVEL TO</p>
-            <p className="text-preset-1">SPACE</p>
-            <p className="text-preset-9 text-blue-300">
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </section>
-          <div className="home__button-container">
-            <Link to="/destination">
-              <button className="home__button text-preset-4">EXPLORE</button>
-            </Link>
-          </div>
-        </div>
+    <div className="home__container">
+      <section className="home__content">
+        <p className="home__subtitle ">SO, YOU WANT TO TRAVEL TO</p>
+        <p className="home__title">SPACE</p>
+        <p className="home__description">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </section>
+      <div className="home__cta">
+        <Link to="/destination" aria-label="Explore space destinations">
+          <button className="home__button text-preset-4">EXPLORE</button>
+        </Link>
+      </div>
+    </div>
   );
 }
