4e69af8da8bf3f76544c778482134f80fd81b92c
aminetiouk <tiouk.amine@gmail.com>
2025-05-12 00:52:43 +0100

    style(title): add page__title class for consistent section heading styling

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -5,7 +5,7 @@ type TitleProps = {
 
 export default function Title({ number, text }: TitleProps) {
   return (
-    <h1 className="text-preset-5">
+    <h1 className="page__title text-preset-5">
       <span className="page__number">{number}</span>{` ${text}`}
     </h1>
   );
