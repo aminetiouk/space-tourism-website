389fe9e96bcb1c7f0665421b460c35af64822722
aminetiouk <tiouk.amine@gmail.com>
2025-05-12 02:03:17 +0100

    feat(title): add optional className prop for flexible styling

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,12 @@
 type TitleProps = {
   number: string;
   text: string;
+  className: string
 }
 
-export default function Title({ number, text }: TitleProps) {
+export default function Title({ number, text, className}: TitleProps) {
   return (
-    <h1 className="page__title text-preset-5">
+    <h1 className={`${className} text-preset-5`}>
       <span className="page__number">{number}</span>{` ${text}`}
     </h1>
   );
