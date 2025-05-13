39d2731147517289988fb1a2b18a53cac9c9ff67
aminetiouk <tiouk.amine@gmail.com>
2025-05-14 00:37:06 +0100

    refactor(navbar): restructure layout with desktop and mobile separation, enhance clarity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,70 +21,69 @@ export default function Navbar({ page }: NavbarProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="navbar">
-      <div className="navbar__container">
-        <Link to="/" aria-label="Home" className="navbar__logo-link">
-          <img
-            src="/shared/logo.svg"
-            alt="Space Tourism Logo"
-            className="navbar__logo"
-          />
-        </Link>
+    <>
+      <nav className="navbar">
+        <div className="navbar__container">
+          <Link to="/" aria-label="Home" className="navbar__logo-link">
+            <img
+              src="/shared/logo.svg"
+              alt="Space Tourism Logo"
+              className="navbar__logo"
+            />
+          </Link>
 
-        <div className="navbar__divider"></div>
-        {/* Hamburger Button */}
-        <button
-          className="navbar__hamburger"
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Toggle menu"
-        >
-          <img
-            src={`${isOpen ? '/shared/icon-close.svg' : '/shared/icon-hamburger.svg'}`}
-            alt="hamburger icon"
-          />
-        </button>
+          <div className="navbar__divider"></div>
 
-        <ul className={`navbar__menu ${isOpen ? 'navbar__menu--open' : ''}`}>
-          {NAV_LINKS.map(({ path, label, code, key }) => (
-            <li key={key} className="navbar__menu-item">
-              <Link
-                to={path}
-                className={`navbar__link ${page === key ? 'navbar__link--active' : ''}`}
-                onClick={() => setIsOpen(false)}
-              >
-                <span className="navbar__link-code">{code}</span> {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-    // {/* <nav className="navbar_mobile">
-    //   <div className="navbar__mobile-container">
-    //     <Link to="/" aria-label="Home" className="navbar__m-logo-link">
-    //       <img
-    //         src="/shared/logo.svg"
-    //         alt="Space Tourism Logo"
-    //         className="navbar__m-logo"
-    //       />
-    //     </Link>
+          <ul className={`navbar__menu text-blue-300 ${isOpen ? 'navbar__menu--open' : ''}`}>
+            {NAV_LINKS.map(({ path, label, code, key }) => (
+              <li key={key} className="navbar__menu-item">
+                <Link
+                  to={path}
+                  className={`navbar__link ${page === key ? 'navbar__link--active' : ''}`}
+                >
+                  <span className="navbar__link-code">{code}</span> {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+      <nav className="navbar__mobile">
+        <div className="navbar__mobile-container">
+          <Link to="/" aria-label="Home" className="navbar__mobile-logo-link">
+            <img
+              src="/shared/logo.svg"
+              alt="Space Tourism Logo"
+              className="navbar__mobile-logo"
+            />
+          </Link>
 
-    //     <button>
+          <button
+            className="navbar__hamburger"
+            onClick={() => setIsOpen(!isOpen)}
+            aria-label="Toggle menu"
+          >
+            <img
+              src={`${isOpen ? '/shared/icon-close.svg' : '/shared/icon-hamburger.svg'}`}
+              alt="hamburger icon"
+            />
+          </button>
 
-    //     </button>
-    //     <ul className="navbar__mobile-menu">
-    //       {NAV_LINKS.map(({ path, label, code, key }) => (
-    //         <li key={key} className="navbar__mobile-menu-item">
-    //           <Link
-    //             to={path}
-    //             className={`navbar__mobile-link ${page === key ? 'navbar__mobile-link--active' : ''}`}
-    //           >
-    //             <span className="navbar__mobile-link-code">{code}</span> {label}
-    //           </Link>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    // </nav> */}
+          <ul className={`navbar__mobile-menu ${isOpen ? 'navbar__menu-mobile--open' : ''}`}>
+            {NAV_LINKS.map(({ path, label, code, key }) => (
+              <li key={key} className="navbar__mobile-menu-item">
+                <Link
+                  to={path}
+                  className={`navbar__mobile-link ${page === key ? 'navbar__mobile-link--active' : ''}`}
+                  // onClick={() => setIsOpen(false)}
+                >
+                  <span className="navbar__mobile-link-code">{code}</span> {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
