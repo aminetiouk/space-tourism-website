5bd484799cc51453e96c81a2258a2a662144b15b
aminetiouk <tiouk.amine@gmail.com>
2025-05-14 22:57:17 +0100

    feat(types): add TCrewMember and TDestination interfaces

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -3,6 +3,23 @@ declare module '*.json' {
   export default value;
 }
 
-interface ImportMeta {
-  glob: (pattern: string, options?: any) => Record<string, string>;
+export interface TCrewMember {
+  name: string;
+  role: string;
+  bio: string;
+  images: {
+    webp: string;
+    png: string;
+  };
+}
+
+export interface TDestination {
+  name: string;
+  description: string;
+  distance: string;
+  travel: string;
+  images: {
+    png: string;
+    webp: string;
+  };
 }
